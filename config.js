fb8ac12c451e6062fd7eed84f06730fc5e0aa1f6
guardmansduck <100035465@mvla.net>
2025-09-05 13:54:51 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js (PUBLIC)
+// These values are safe to expose — they will be visible in the browser.
+const CONFIG = {
+  CLIENT_ID: "Ov23li3Fit4bt8BOD31D", 
+  REDIRECT_URI: "https://guardmansduck.github.io/GitOS"
+};
